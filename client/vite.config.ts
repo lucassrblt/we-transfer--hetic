4e5b9f6213c3d1f56cf65c7bb9fb7aa2f1305f93
lucas.rimbault <lucas.rimbault@tentelemed.com>
2024-10-30 12:08:03 +0100

    Adding dockerfiles

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,12 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsConfigPaths()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true
+    }
+  }
 })
